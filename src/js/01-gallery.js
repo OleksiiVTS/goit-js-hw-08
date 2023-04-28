d7718f7868a7ad9f232bc587336101405f2c9b67
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-04-29 01:28:03 +0300

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const ulGallery = document.querySelector(".gallery");
+const createMakup = createFotoGalery(galleryItems);
+
+ulGallery.insertAdjacentHTML('beforeend', createMakup);
+
+function createFotoGalery(galleryItems) {
+    return galleryItems
+        .map(({description, original, preview}) => {
+        return `
+        <li class="gallery__item">
+        <a 
+            class="gallery__link" 
+            href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>
+        </li>
+        `;
+        })
+        .join(""); 
+};
+
+new SimpleLightbox('.gallery a', { 
+    /* options */ 
+    captionsData:'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
+
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const secondNow = localStorage.getItem("videoplayer-current-time");
+
+
+const timeListenerSecond = function(data) {
+    player.getCurrentTime().then(function(seconds) {
+        // _.throttle(
+        localStorage.setItem("videoplayer-current-time", seconds),
+        // 1000);
+        console.log(seconds);
+    }).catch(function(error) {
+    });
+};
+
+player.on('timeupdate', _.throttle(timeListenerSecond, 1000));
+
+player.setCurrentTime(secondNow).then(function(seconds) {})
+.catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
+
+console.log(secondNow)
