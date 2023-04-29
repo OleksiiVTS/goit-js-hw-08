3739ed0d795893dd8d15bfb02ec56c9703ca8b1b
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-04-29 12:25:35 +0300

    +

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import _ from 'lodash';
 
+const form = document.querySelector(".feedback-form");
+const formInput = form.addEventListener('input', _.throttle(saveValueForm, 500));
+// const formInput = form.addEventListener('input', saveValueForm);
+const formSubmit = form.addEventListener('submit', removeValueForm);
+const LOCALSTORAGE_KEY = "feedback-form-state";
+let saveValue = {
+    email: "",
+    message: "",
+};
+
+getToLocalStorare();
+
+function saveValueForm(event) {
+    event.preventDefault()
+    const {
+        elements: { email, message }
+    } = event.currentTarget;
+    setToLocalStorare(email.value, message.value);
+    getToLocalStorare();
+};
+
+function setToLocalStorare(email, message) {
+    saveValue.email = email;
+    saveValue.message = message;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(saveValue));
+};
+
+function getToLocalStorare() {
+    const getInfo = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    console.log(getInfo)
+    if (getInfo === null) {
+        form.email.value = "";
+        form.message.value = "";
+    } else {
+        form.email.value = getInfo.email;
+        form.message.value = getInfo.message;
+    };
+};
+
+function removeValueForm(event) {
+    event. preventDefault()
+    console.log(form.email.value)
+    console.log(form.message.value)
+    event.currentTarget.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
